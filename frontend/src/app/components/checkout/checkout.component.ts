6cb0722501edaaf9bfd643468406a927422080ce
Chri2704 <angilerichristian439@gmail.com>
2023-07-13 08:53:52 +0200

    checkout aggiunto

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../../services/cart.service";
+import {CartModelServer} from "../../models/cart.model";
+import {Router} from "@angular/router";
+import {OrderService} from "../../services/order.service";
+import {NgxSpinnerService} from "ngx-spinner";
+import {FormBuilder, NgForm, Validators} from "@angular/forms";
+import { productModelServer } from 'src/app/models/product.model';
 
 @Component({
-  selector: 'app-checkout',
+  selector: 'mg-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  cartData: CartModelServer= {
+    total: 0,
+    data: [{
+      product:{} as productModelServer,
+      numInCart:0,
+    }]
+  };
+  cartTotal: number = 0;
+ //showSpinner: boolean;
+  checkoutForm: any;
+  constructor(private cartService: CartService,
+              private orderService: OrderService,
+              private router: Router,
+              private  spinner: NgxSpinnerService,
+              private fb: FormBuilder) {
 
+    this.checkoutForm = this.fb.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+
+    });
+
+
+  }
+
+  ngOnInit() {
+    this.cartService.cartData$.subscribe(data => this.cartData = data);
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
+
+  }
+
+  onCheckout() {
+   this.spinner.show().then(p => {
+      this.cartService.CheckOutFromCart(2);
+    });
+
+
+  //console.log(this.checkoutForm.value);
+
+  }
 }
